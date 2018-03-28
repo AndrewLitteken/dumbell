2c00ca55a7369b0675fb327e66aac1ced8e1c7f7
Andrew Litteken <andrewlitteken@gmail.com>
2018-03-27 23:17:30 -0700

    adding redefinition

--- src/Symbol.cpp
+++ src/Symbol.cpp
@@ -14,12 +14,12 @@ Symbol::Symbol(symbol_t k, bool def, std::string n, Expr *e, Type *t,
     struct iter_info *initial_info = (struct iter_info *)
         malloc(sizeof(struct iter_info));
     initial_info->def_or_expr = def;
-    initial_info->label = "D"+std::to_string(overall_def_num);
     initial_info->definition = e;
     initial_info->type = t;
     dependents = new std::vector<std::string>;
     iter_info.push_back(initial_info);
     if(def) {
+        initial_info->label = "D"+std::to_string(overall_def_num);
         overall_def_num++;
         std::set<std::string> dependencies(*process_def());
         add_dependents(&dependencies, table);
@@ -33,7 +33,7 @@ Symbol::Symbol(std::string n, std::string d){
 }
 
 std::set<std::string> *Symbol::process_def(){
-    return iter_info[0]->definition->get_expression_deps();
+    return (*(iter_info.end() - 1))->definition->get_expression_deps();
 }
 
 void Symbol::add_dependents(std::set<std::string> *dependencies,
@@ -52,8 +52,18 @@ void Symbol::add_dependents(std::set<std::string> *dependencies,
     }
 }
 
-void Symbol::redefine(symbol_t s, Expr* e, Type *t){
+void Symbol::redefine(symbol_t s, bool def, Expr* e, Type *t, SymbolTable *table){
     def_number++;
-    overall_def_num++;
-    
+    struct iter_info *new_info = (struct iter_info *)
+        malloc(sizeof(struct iter_info));
+    new_info->def_or_expr = def;
+    new_info->definition = e;
+    new_info->type = t;
+    iter_info.push_back(new_info);
+    if(def){
+        new_info->label = "D"+std::to_string(overall_def_num);
+        overall_def_num++;
+        std::set<std::string> dependencies(*process_def());    
+        add_dependents(&dependencies, table); 
+    }
 }
